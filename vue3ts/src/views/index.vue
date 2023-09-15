c8718eea77b9a6c1a8cbbb3fd90698e11a0c34ed
liwei1dao <liwei1dao@gmail.com>
2023-09-15 10:17:47 +0800

    上传代码

--- vue3ts/src/views/index.vue
+++ vue3ts/src/views/index.vue
@@ -1,16 +1,11 @@
 <template>
   <v-container>
     <v-row justify-center>
-      <v-col cols="12">
+      <v-col cols="3">
         <div class="bg-red-500">
           <h1 className="text-3xl text-center font-bold underline">
             Hello world!
           </h1>
-        </div>
-        <div class="flex flex-nowrap">
-          <div class="basis-1/2">01</div>
-          <div class="basis-1/2">02</div>
-          <div class="basis-1/2">03</div>
         </div>
       </v-col>
     </v-row>
